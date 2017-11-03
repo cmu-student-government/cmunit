64ba0f821be14c3bca7583007e8d36220f1526a7
Marat <valiev.m@gmail.com>
2017-11-03 14:17:30 -0400

    refactor: convert bookmarklet to extension style

--- sio_plus/content.js
+++ sio_plus/content.js
@@ -36,94 +36,86 @@ object. Everything has to be in plain vanilla javascript.
 
  */
 
+var data, // fce data
+  course_total = 0,  // sum of total course workload
+  observer;  // listener of DOM change events
 
-var sio_plus = sio_plus || {
-  data_url: "https://cmu-student-government.github.io/fce-data/fce.json",
-  data: null,
-  course_total: 0,  // sum of total course workload
-  log_level: null,
-  observer: null,  // listener to DOM event changes
+const DEBUG=1, INFO=1, WARNING=3, ERROR=4, CRITICAL=5;
+var log_level = DEBUG;
 
-  log: function(message, level) {
-    const DEBUG=1, INFO=1, WARNING=3, ERROR=4, CRITICAL=5;
-    level = level || DEBUG;
-    if (level >= sio_plus.loglevel) console.log(message);
-  },
+function log(message, level) {
+  level = level || DEBUG;
+  if (level >= log_level) console.log(message);
+}
 
-  init: function(loglevel){
-    sio_plus.loglevel = loglevel || 3;
-    if (sio_plus.data) return;
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", sio_plus.data_url, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        sio_plus.data = JSON.parse(xhr.responseText);
+function updateCourseTotal(){
+  if (course_total === 0) return;
+  course_total = Math.round(course_total * 100) / 100;
+  var node = document.getElementsByClassName("schedule-units-label")[0];
+  log(node);
+  if (!node) return;
+  node.insertBefore(new Text(" (actually " + course_total + ")"), node.lastChild);
+  course_total = 0;
+}
 
-        window.addEventListener('hashchange', sio_plus.onHashChange);
+function onDOMChange (mutations) {
+  // label to match: "15-780 :: 12.0 units"
+  var id_re = /(\d\d)-(\d\d\d)\s*::\s*(\d+)/;
 
-        sio_plus.log("FCE data loaded");
+  mutations.forEach(function(mutation) {
+    // Some GWT event fired, and as a result of RPC call some nodes are
+    // attached or removed. here we look at the content of these nodes and
+    // try to understand what happened
+
+    mutation.addedNodes.forEach(function(node) { // added nodes
+      if (node.lastChild && node.lastChild.classList) {
+        log(node);
+        node.lastChild.classList.forEach(function(cls) {
+          switch (cls) {
+            case "gwt-course": // course in the right column
+              var units_node = node.querySelectorAll(".course-units")[0];
+              var match = units_node && id_re.exec(units_node.innerHTML);
+              if (match) {
+                var hours = data[match[1] + match[2]];
+                hours = hours && hours["hrs"];
+                if (hours)
+                  units_node.innerHTML += " (avg spent " + hours + ")";
+                course_total += (hours || parseInt(match[3]));
+              }
+              break;
+
+            case "gwt-appointment": // course in the calendar view, rendered last
+              updateCourseTotal();
+              break;
+          }
+        });
       }
-    };
-    xhr.send();
-  },
-
-  updateCourseTotal: function(){
-    if (sio_plus.course_total === 0) return;
-    sio_plus.course_total = Math.round(sio_plus.course_total * 100) / 100;
-    var node = document.getElementsByClassName("schedule-units-label")[0];
-    sio_plus.log(node);
-    if (!node) return;
-    node.insertBefore(new Text(" (actually " + sio_plus.course_total + ")"), node.lastChild);
-    sio_plus.course_total = 0;
-  },
-
-  onDOMChange: function(mutations) {
-    // label to match: "15-780 :: 12.0 units"
-    var id_re = /(\d\d)-(\d\d\d)\s*::\s*(\d+)/;
-
-    mutations.forEach(function(mutation) {
-      // Some GWT event fired, and as a result of RPC call some nodes are
-      // attached or removed. here we look at the content of these nodes and
-      // try to understand what happened
-
-      mutation.addedNodes.forEach(function(node) { // added nodes
-        if (node.lastChild && node.lastChild.classList) {
-          sio_plus.log(node);
-          node.lastChild.classList.forEach(function(cls) {
-            switch (cls) {
-              case "gwt-course": // course in the right column
-                var units_node = node.querySelectorAll(".course-units")[0];
-                var match = units_node && id_re.exec(units_node.innerHTML);
-                if (match) {
-                  var hours = sio_plus.data[match[1] + match[2]];
-                  hours = hours && hours["hrs"];
-                  if (hours)
-                    units_node.innerHTML += " (avg spent " + hours + ")";
-                  sio_plus.course_total += (hours || parseInt(match[3]));
-                }
-                break;
-
-              case "gwt-appointment": // course in the calendar view, rendered last
-                sio_plus.updateCourseTotal();
-                break;
-            }
-          });
-        }
-      });
     });
-  },
+  });
+}
 
-  onHashChange: function(event) {
-    if (!sio_plus.observer) { // defer load
-      var target = document.getElementsByClassName('body-content')[0];
-      sio_plus.log(target);
+function onHashChange(event) {
+  course_total = 0;
+}
 
-      sio_plus.observer = new MutationObserver(sio_plus.onDOMChange);
-      sio_plus.observer.observe(target, {childList: true, subtree: true});
-      sio_plus.log("Mutations observer installed");
-    }
+// initialize data
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://cmu-student-government.github.io/fce-data/fce.json", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    data = JSON.parse(xhr.responseText);
+
+    window.addEventListener('hashchange', onHashChange);
+
+    log("FCE data loaded");
   }
 };
+xhr.send();
 
-sio_plus.init();
+document.addEventListener('DOMContentLoaded', function(event){
+  var target = document.body;
+  observer = new MutationObserver(onDOMChange);
+  observer.observe(target, {childList: true, subtree: true});
+  log("Mutations observer installed");
+});
